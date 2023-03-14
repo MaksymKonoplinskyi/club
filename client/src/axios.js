ac7efe9acf0a6d4169b643d6120238962847e7c1
MaksymKonoplinskyi <gran7777777@gmail.com>
2023-03-14 20:37:09 +0200

    All good

--- client/src/axios.js
+++ client/src/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL
 })
- 
+
 instance.interceptors.request.use(config => {
     config.headers.Authorization = window.localStorage.getItem('token')
     return config
